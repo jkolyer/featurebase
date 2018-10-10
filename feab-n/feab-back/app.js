6cbe62ea2ab675ef4c25749b6193136a1b492e1a
Jonathan Kolyer <cohere.us@gmail.com>
2018-10-10 16:43:17 +0000

    mocha:  reorg file structure

--- feab-n/feab-back/app.js
+++ feab-n/feab-back/app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app/controllers/routes/index');
+var usersRouter = require('./app/controllers/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
