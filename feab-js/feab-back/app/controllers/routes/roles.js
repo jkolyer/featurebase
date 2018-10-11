b40c9e8e238fb79eda55f5d2ca22e893f6c756dc
Jonathan Kolyer <cohere.us@gmail.com>
2018-10-11 20:15:16 +0000

    eslint on app/controllers/routes/roles.js

--- feab-js/feab-back/app/controllers/routes/roles.js
+++ feab-js/feab-back/app/controllers/routes/roles.js
@@ -1,17 +1,15 @@
-
-let mongoose = require('mongoose');
-let Role = require('../../models/role');
+const Role = require('../../models/role');
 
 /*
  * GET /role route to retrieve all the roles.
  */
 function getRoles(req, res) {
-    //Query the DB and if no errors, send all the roles
-    let query = Role.find({});
+    // Query the DB and if no errors, send all the roles
+    const query = Role.find({});
     query.exec((err, roles) => {
-	if(err) res.send(err);
-	//If no errors, send them back to the client
-	res.json(roles);
+        if (err) res.send(err);
+        // If no errors, send them back to the client
+        res.json(roles);
     });
 }
 
@@ -19,16 +17,15 @@ function getRoles(req, res) {
  * POST /role to save a new role.
  */
 function postRole(req, res) {
-    //Creates a new role
-    var newRole = new Role(req.body);
-    //Save it into the DB.
-    newRole.save((err,role) => {
-	if(err) {
-	    res.send(err);
-	}
-	else { //If no errors, send it back to the client
-	    res.json({message: "Role successfully added!", role });
-	}
+    // Creates a new role
+    const newRole = new Role(req.body);
+    // Save it into the DB.
+    newRole.save((err, role) => {
+        if (err) {
+            res.send(err);
+        } else { // If no errors, send it back to the client
+            res.json({ message: 'Role successfully added!', role });
+        }
     });
 }
 
@@ -37,9 +34,9 @@ function postRole(req, res) {
  */
 function getRole(req, res) {
     Role.findById(req.params.id, (err, role) => {
-	if(err) res.send(err);
-	//If no errors, send it back to the client
-	res.json(role);
+        if (err) res.send(err);
+        // If no errors, send it back to the client
+        res.json(role);
     });
 }
 
@@ -47,8 +44,8 @@ function getRole(req, res) {
  * DELETE /role/:id to delete a role given its id.
  */
 function deleteRole(req, res) {
-    Role.remove({_id : req.params.id}, (err, result) => {
-	res.json({ message: "Role successfully deleted!", result });
+    Role.remove({ _id: req.params.id }, (err, result) => {
+        res.json({ message: 'Role successfully deleted!', result });
     });
 }
 
@@ -56,14 +53,16 @@ function deleteRole(req, res) {
  * PUT /role/:id to updatea a role given its id
  */
 function updateRole(req, res) {
-    Role.findById({_id: req.params.id}, (err, role) => {
-	if(err) res.send(err);
-	Object.assign(role, req.body).save((err, role) => {
-	    if(err) res.send(err);
-	    res.json({ message: 'Role updated!', role });
-	});
+    Role.findById({ _id: req.params.id }, (err, role) => {
+        if (err) res.send(err);
+        Object.assign(role, req.body).save((err2, role2) => {
+            if (err2) res.send(err2);
+            res.json({ message: 'Role updated!', role2 });
+        });
     });
 }
 
-//export all the functions
-module.exports = { getRoles, postRole, getRole, deleteRole, updateRole };
+// export all the functions
+module.exports = {
+    getRoles, postRole, getRole, deleteRole, updateRole,
+};
