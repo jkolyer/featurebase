a17e2af318f1a1569120b806f5f9b3200973e830
Jonathan Kolyer <cohere.us@gmail.com>
2018-10-23 06:13:12 +0000

    getting login stubs to work

--- feab-js/feab-saas/back-end/__tests__/server/routes/domain.ts
+++ feab-js/feab-saas/back-end/__tests__/server/routes/domain.ts
@@ -2,49 +2,106 @@
 process.env.NODE_ENV = 'test';
 
 // Require the dev-dependencies
-import { server } from '../../../server/app';
+import { app } from '../../../server/app';
 import { Domain } from '../../../server/models/Domain';
-import { buildDomain } from '../../utils/domainBuilders'
+import { DomainRole } from '../../../server/models/DomainRole';
+import { buildDomain, owner } from '../../utils/domainBuilders'
 import * as mongoose from 'mongoose';
 import * as request from 'supertest';
 
+const serverAgent = request.agent(app);
+
 import { logger } from '../../../server/utils/logs';
 
-// import chai from 'chai';
-// const should = chai.should();
+import * as sinon from "sinon";
+import * as passport from 'passport';
 
 // Our parent block
 describe('Domains', () => {
+  
   beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URL_TEST);
+    
+    await Domain.remove({});
+    await DomainRole.remove({});
+    
+    // this.authenticate = sinon.stub(passport,"authenticate").returns(() => {});
+
+    this.authenticate = sinon.stub(passport,"authenticate")
+      .callsFake((strategy, options) => {
+        // debugger
+        logger.debug(`*** callsFake:  ${strategy}; ${options}`);
+        // if (callback) {
+        //   callback(null, { "username": "test@techbrij.com"}, null);
+        // }
+        return (req, res, next) => {
+          if (req && res && next) {
+            req.user = this.user;
+            logger.debug('*** authenticate: ');
+            res.redirect(`/`);
+          }
+          // next();
+        };
+      });
   });
   
-  beforeEach(async done => { // Before each test we empty the database
+  afterAll(async () => {
+    this.authenticate.restore();
+  });
+  
+  afterEach(async () => {
     await Domain.remove({});
+    await DomainRole.remove({});
+  });
+  
+  beforeEach(async done => {
+    await buildDomain('Site');
+    await buildDomain('Adhoc');
+
+    this.user = await owner()
+    this.userId = this.user.id;
+
+    // this.authenticate.yields(0, { id: this.userId });
+    // this.authenticate.yields({ id: this.userId });
+    // this.authenticate.yields(null, { id: this.userId });
+    
     done();
   });
 
+  function loginUser() {
+    return function(done) {
+      serverAgent
+        .get('/auth/google')
+        .send({ username: 'admin', password: 'admin' })
+        .expect(302)
+        .expect('Location', '/')
+        .end(onResponse);
+
+      function onResponse(err, res) {
+        if (err) return done(err);
+        logger.debug(`*** onResponse:  ${res}`);
+        return done();
+      }
+    };
+  };  
   /*
    * Test the /GET route
    */
   describe('/GET domain', () => {
-    it('it should GET all the domains', async done => {
+    test('login', loginUser());
+    /*
+    test('it should GET all the domains', async done => {
       await buildDomain('Site');
       await buildDomain('Adhoc');
+
+      const response = await request(serverAgent)
+        .get('/api/v1/team-leader/teams/get-members');
       
-      request(server)
-        .get('/domains')
-        .end((err, res) => {
-          debugger
-          expect(err).toBeNull();
-          logger.debug(`*** /get:  ${res}`);
-          
-          expect(res.statusCode).toEqual(200);
-          // res.should.have.status(200);
-          // res.body.should.be.a('array');
-          // res.body.length.should.be.eql(2);
-          done();
-        });
+      // const response = await request(app).get('/api/v1/domains');
+      logger.debug(`*** /get:  ${response}`);
+      expect(response.statusCode).toBe(200);
+      done();
     });
+    */
   });
 });
